e5f57e953014e8bad42904574be80d8adfbeaba3
Kouta3366 <patukin4040@gmail.com>
2018-12-13 16:27:24 +0900

    121316

--- post-app/config/routes.rb
+++ post-app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'posts/new'
   get 'users/login_form'
   post "login" => "users#login"
-  post "logout" => "users#logout"
+  get "logout" => "users#logout"
   get 'home/top'
   get "signup" => "users#new"
   post "users/create" => "users#create"
